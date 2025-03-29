57e7630ac58a4e4c973346653f4e189c6c91583b
Jianing Wang <jwan0342@student.monash.edu>
2025-03-29 22:09:13 +1100

    Config root .gitignore file for the  nodeBackEnd/node_modules/.

---
+++ nodeBackEnd/server.js
@@ -0,0 +1,22 @@
+console.log("Backend server is running");
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// let reviews = [];
+
+// app.get('/reviews', (req, res) => {
+//     res.json(reviews);
+// });
+
+// app.post('/addReview', (req, res) => {
+//     reviews.push(req.body);
+//     res.json({ success: true });
+// });
+
+// app.listen(3000, () => {
+//     console.log('Backend running on http://localhost:3000');
+// });
