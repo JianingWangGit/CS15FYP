54bca8cb76137effd6b1d1af7a941d7ba15a0239
Abdus Sami <asam0025@student.monash.edu>
2025-04-14 22:32:58 +1000

    Nav Bar
    - Adding components for a navigation bar

--- app/src/main/java/com/example/cs_15_fyp/MainActivity.java
+++ app/src/main/java/com/example/cs_15_fyp/MainActivity.java
@@ -7,19 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.cs_15_fyp.databinding.ActivityMainBinding;
+import com.example.cs_15_fyp.temp_frag.NotificationsFragment;
+import com.example.cs_15_fyp.temp_frag.ProfileFragment;
+import com.example.cs_15_fyp.temp_frag.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        replaceFragment(new SearchFragment());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.bottomNavigationView.setOnItemSelectedListener( item -> {
+
+            int id = item.getItemId();
+
+            if (id == R.id.notifications) {
+                replaceFragment(new NotificationsFragment());
+                return true;
+            } else if (id == R.id.search) {
+                replaceFragment(new SearchFragment());
+                return true;
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+                return true;
+            } else {
+                return false;
+            }
+
+        });
     }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
 }
