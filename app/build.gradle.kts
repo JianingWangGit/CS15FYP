2d3009b80171c3ccb9e1c21bc52e2eefe92ce5df
kmor0044 <kmor0044@student.monash.edu>
2025-04-29 16:46:37 +1000

    login + signup doesn't include separation between users yet.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-}
+    alias(libs.plugins.google.gms.google.services)
+  }
 
 android {
     namespace = "com.example.cs_15_fyp"
@@ -37,7 +38,25 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    //implementation("com.google.firebase:firebase-bom:31.5.0")
+    //implementation("com.google.firebase:firebase-auth-ktx")
+    //implementation("com.google.android.gms:play-services-auth:20.5.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.credentials:credentials:1.5.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation ("com.google.firebase:firebase-firestore:25.1.4")
 }
---
+++ app/src/main/java/com/example/cs_15_fyp/SignUpActivity.java
@@ -0,0 +1,155 @@
+package com.example.cs_15_fyp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private FirebaseFirestore fstore;
+     private EditText signupEmail, signupPassword;
+
+    private Spinner usertypeSpinner;
+    public Boolean usertype;
+
+    private Button signupButton;
+    private TextView loginRedirectText;
+    //private ImageView signupGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        auth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        //using spinner
+        usertypeSpinner = findViewById(R.id.usertype_spinner);
+
+        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_type, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        usertypeSpinner.setAdapter(adapter);
+        // Set a default selection that acts as a prompt
+       usertypeSpinner.setSelection(0);
+
+        //TODO: google signin
+        //signupGoogle = findViewById(R.id.signup_google);
+
+
+        /*signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+                //String usertype = usertypeSpinner.getSelectedItem().toString().trim();
+
+                //account creation
+                if (user.isEmpty()){
+                    signupEmail.setError("Email cannot be empty");
+                }
+                if (pass.isEmpty()){
+                    signupPassword.setError("Password cannot be empty");
+                } else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this, "Sign up Successful", Toast.LENGTH_SHORT).show();
+                                //startActivity(new Intent(SignUpActivity.this, RestaurantSearchActivity.class));
+                                startActivity(new Intent(String.valueOf(SignUpActivity.this))); //TODO :remove after merge
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "Sign up Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });*/
+
+        signupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String user = signupEmail.getText().toString().trim();
+                String pass = signupPassword.getText().toString().trim();
+                String usertype_spinner = usertypeSpinner.getSelectedItem().toString().trim();
+
+                if (usertypeSpinner.getSelectedItemPosition() == 0) {
+                    // First item is the prompt, so selection isn't valid
+                    Toast.makeText(SignUpActivity.this, "Please make a user type selection", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    /*if (usertypeSpinner.getSelectedItemPosition() == 1){
+                        usertype = true; //personal account
+                    } else if (usertypeSpinner.getSelectedItemPosition() == 2) {
+                        usertype = false; //business account
+                    }*/
+
+                    //account creation
+                    if (user.isEmpty()) {
+                        signupEmail.setError("Email cannot be empty");
+                    }
+                    if (pass.isEmpty()) {
+                        signupPassword.setError("Password cannot be empty");
+                    } else {
+                        auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUpActivity.this, "Sign up Successful", Toast.LENGTH_SHORT).show();
+
+                                    FirebaseUser fuser = auth.getCurrentUser();
+                                    DocumentReference df   = fstore.collection("Users").document(fuser.getUid());
+                                    Map<String,Object> userInfo = new HashMap<>();
+                                    userInfo.put("Email", signupEmail.getText().toString().trim());
+                                    userInfo.put("Personal", usertypeSpinner.getSelectedItemPosition());
+                                    df.set(userInfo);
+
+                                    startActivity(new Intent(String.valueOf(SignUpActivity.this))); //TODO :remove after merge
+                                    //startActivity(new Intent(SignUpActivity.this, RestaurantSearchActivity.class));
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "Sign up Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
