561b5334ae72f6989ca65ba9522034ae79da3f53
Abdus Sami <asam0025@student.monash.edu>
2025-05-11 14:49:15 +1000

    Reply Separation
    Made the following changes:
    - Separated the model, router, and API from reviews
    - Replies are now stored in their own document

--- nodeBackEnd/models/review_model.js
+++ nodeBackEnd/models/review_model.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
 
-const replySchema = new mongoose.Schema({
-    email: String,
-    username: String,
-    comment: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
 const reviewSchema = new mongoose.Schema({
     restaurantId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -20,7 +13,6 @@ const reviewSchema = new mongoose.Schema({
         required: true,
     },
     photos: [String],
-    replies: [replySchema],
     createdAt: { type: Date, default: Date.now }
 });
 
